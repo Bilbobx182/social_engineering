8cede7d5d0ecf0b585c8283c2629b9f13a4bb17e
Ciaran O Nuallain <onuallainc@gmail.com>
2021-11-19 12:06:32 +0000

    https working

--- code/main.py
+++ code/main.py
@@ -1,6 +1,5 @@
 import json
-from flask import Flask, render_template
-
+from flask import Flask, render_template,request,redirect
 app = Flask(__name__)
 
 # Very secure "database" lol that just stores the count of times this page was accessed.
@@ -18,5 +17,14 @@ def get_count():
 def route():
     return render_template('template.html', person_num=get_count())
 
+
+# If you're copying + pasting, don't do this in a real production.
+# Use something like nginx.
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        return redirect(url, code=302)
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", ssl_context=('./ssl/cert.pem', './ssl/privkey.pem'))
+    app.run(host="0.0.0.0", port=443, ssl_context=('/ssl/cert.pem', '/ssl/privkey.pem'))
